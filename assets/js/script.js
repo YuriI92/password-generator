cab9ecd6eb32f16b07b95e924e17ddde662761ba
Yurippe <yurichikawa1992@gmail.com>
2022-03-08 22:36:15 -0500

    add getting min/max function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,33 @@
 // Assignment code here
 
-
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
+
+var getMinLength = function() {
+  var minLength = prompt("How much is the minimum character length?");
+
+  if (minLength === "" || minLength === null) {
+    alert("The answer is not valid. Try again.");
+    getMinLength();
+  } else {
+    console.log(minLength);
+    return minLength;
+  }
+};
+
+var getMaxLength = function() {
+  var maxLength = prompt("How much is the maximum character length?");
+
+  if (maxLength === "" || maxLength === null) {
+    alert("The answer is not valid. Try again.");
+  } else {
+    console.log(maxLength);
+    return maxLength;
+  }
+};
+
+getMinLength();
+getMaxLength();
 
 // Write password to the #password input
 function writePassword() {
@@ -14,4 +39,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
