7384daee946d748fe205762ae9d1447589ff44a2
Yurippe <yurichikawa1992@gmail.com>
2022-03-09 19:01:39 -0500

    add ability to ask criteria to button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,70 @@
 // Assignment code here
 
 // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
-var getMinLength = function() {
-  var minLength = prompt("How much is the minimum character length?");
-  var toNum = Number(minLength);
-
-  if (toNum === "" || toNum === null) {
-    alert("The answer is not valid. Try again.");
-    getMinLength();
-  } else {
-    console.log(toNum);
-    return toNum;
+generateBtn.addEventListener("click", function(){
+  var getMinLength = function() {
+    var minLength = prompt("How much is the minimum character length?");
+    var toNum = Number(minLength);
+  
+    if (toNum === "" || toNum === null) {
+      alert("The answer is not valid. Try again.");
+      getMinLength();
+    } else {
+      console.log(toNum);
+      return toNum;
+    }
+  };
+  
+  var minLength = getMinLength();
+  
+  var getMaxLength = function() {
+    var maxLength = prompt("How much is the maximum character length?");
+    var toNum = Number(maxLength);
+  
+    if (toNum === "" || toNum === null) {
+      alert("The answer is not valid. Try again.");
+      getMaxLength();
+    } else if (toNum < minLength) {
+      alert("Please enter a number larger than the minimum character length. Try again.");
+      getMaxLength();
+    } else {
+      console.log(toNum);
+      return toNum;
+    }
+  };
+  
+  var maxLength = getMaxLength();
+  
+  var lowCase = "lower case letters";
+  var upCase = "upper case letters";
+  var numChar = "numbers";
+  var specChar = "special characters";
+  
+  var charType = [lowCase, upCase, numChar, specChar];
+  
+  var confChar = function(i) {
+    var storeConf = confirm("Do you want to include " + charType[i] + "?");
+  
+    if (storeConf) {
+      return "yes";
+    } else {
+      return "no";
+    }
   }
-};
-
-var minLength = getMinLength();
-
-var getMaxLength = function() {
-  var maxLength = prompt("How much is the maximum character length?");
-  var toNum = Number(maxLength);
-
-  if (toNum === "" || toNum === null) {
-    alert("The answer is not valid. Try again.");
-    getMaxLength();
-  } else if (toNum < minLength) {
-    alert("Please enter a number larger than the minimum character length. Try again.");
-    getMaxLength();
-  } else {
-    console.log(toNum);
-    return toNum;
+  
+  var passwordCriteria = {
+    minLength: minLength,
+    maxLength: maxLength,
+    lowCase: confChar(0),
+    upCase: confChar(1),
+    numChar: confChar(2),
+    specChar: confChar(3),
   }
-};
-
-var maxLength = getMaxLength();
-
-var lowCase = "lower case letters";
-var upCase = "upper case letters";
-var numChar = "numbers";
-var specChar = "special characters";
-
-var charType = [lowCase, upCase, numChar, specChar];
-
-var confChar = function(i) {
-  var storeConf = confirm("Do you want to include " + charType[i] + "?");
-
-  if (storeConf) {
-    return "yes";
-  } else {
-    return "no";
-  }
-}
-
-var passwordCriteria = {
-  minLength: minLength,
-  maxLength: maxLength,
-  lowCase: confChar(0),
-  upCase: confChar(1),
-  numChar: confChar(2),
-  specChar: confChar(3),
-}
-
-console.dir(passwordCriteria);
+  
+  console.dir(passwordCriteria);
+});
 
 // Write password to the #password input
 function writePassword() {
