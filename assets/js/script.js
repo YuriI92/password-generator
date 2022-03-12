065345f7ad4846ae468d036812e5ad316e6d4aa1
Yurippe <yurichikawa1992@gmail.com>
2022-03-11 20:07:17 -0500

    correct the password length criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,49 +7,22 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", function(){
   // generate random password by requesting user for a criteria
   var generatePassword = function() {
-    // get minimum length
-    var getMinLength = function() {
-      var minLength = prompt("How much is the minimum character length?");
 
-      // if user enter invalid value, let user try again
-      if (minLength === "" || minLength === null || isNaN(minLength) === true) {
-        alert("The answer is not valid. Try again.");
-        getMinLength();
-      } else {
-        // change to numeric type
-        minLength = Number(minLength);
-        return minLength;
-      }
-    };
-      
-    var minLength = getMinLength();
-    
-    // get maximum length
-    var getMaxLength = function() {
-      var maxLength = prompt("How much is the maximum character length?");
-    
-      // if user enter invalid value, let user try again
-      if (maxLength === "" || maxLength === null || isNaN(maxLength) === true) {
-        alert("The answer is not valid. Try again.");
-        getMaxLength();
-      // if maxLength was below minLength, let user try again
-      } else if (maxLength < minLength) {
-        alert("Please enter a number larger than the minimum character length. Try again.");
-        getMaxLength();
-      } else {
-        maxLength = Number(maxLength);
-        return maxLength;
-      }
-    };
+    var length = prompt("Enter the length of a password between 8 and 128.");
 
+    if (!(length >= 8 && length <= 128)) {
+      alert("The answer is not valid. Please enter the number between 8 and 128.");
+      return "";
+    }
+    
     // define each characters
-    var lowCase = "lower case letters";
-    var upCase = "upper case letters";
-    var numChar = "numbers";
-    var specChar = "special characters";
+    var lowerCase = "lower case letters";
+    var upperCase = "upper case letters";
+    var numbers = "numbers";
+    var special = "special characters";
     
     // make characters to a set so that confChar function repeat until the end of character types
-    var charType = [lowCase, upCase, numChar, specChar];
+    var charType = [lowerCase, upperCase, numbers, special];
     
     // get which character types user want to use
     var confChar = function(i) {
@@ -62,13 +35,10 @@ generateBtn.addEventListener("click", function(){
       }
     }
     
-    var maxLength = getMaxLength();
-
     // password criteria
     var pswCrit = [
       {
-        min: minLength,
-        max: maxLength
+        length: length,
       },
       {
         lowCase: confChar(0),
@@ -84,13 +54,6 @@ generateBtn.addEventListener("click", function(){
     // create random characters
     var result = '';
 
-    // create random length between minLength and maxLength
-    var randomLength = function(min, max) {
-      var value = Math.floor(Math.random() * (max - min + 1)) + min;
-      console.log(value);
-      return value;
-    }
-
     // create random password
     var randomPassword = function () {
       // define each character types
@@ -98,19 +61,21 @@ generateBtn.addEventListener("click", function(){
       var numbers = '0123456789';
       var special = '!#$%&()=-?+,._'
       
+      console.log(length);
+
       // create random password
       var randomForm = function() {
         // define password length using randomLength function
-        var pswLength = randomLength(minLength, maxLength);
+        // var pswLength = randomLength(minLength, maxLength);
         // set random characters until it reaches pswLength
-        for (var i = 0; i < pswLength; i++) {
+        for (var i = 0; i < length; i++) {
           result += characters.charAt(Math.floor(Math.random() * characters.length));
         }
       }
 
       // check which type user requested to use for password and merge the characters into a single variable
-      if (pswCrit[1].lowCase === "yes") { // with lowcase
-        if (pswCrit[1].upCase === "yes") { //with upcase
+      if (pswCrit[1].lowCase === "yes") { // with lowCase
+        if (pswCrit[1].upCase === "yes") { //with upCase
           if (pswCrit[1].numChar === "yes") { // with num
             if (pswCrit[1].specChar === "yes") { // with spec
               var characters = letters + letters.toUpperCase() + numbers + special;
@@ -123,7 +88,7 @@ generateBtn.addEventListener("click", function(){
               console.log(result);
               return result;
             }
-          } else { // lowcase, upcase, w/o num
+          } else { // lowCase, upCase, w/o num
             if (pswCrit[1].specChar === "yes") { // with spec
               var characters = letters + letters.toUpperCase() + special;
               randomForm();
@@ -136,7 +101,7 @@ generateBtn.addEventListener("click", function(){
               return result;
             }
           }
-        } else { // lowcase, w/o upcase
+        } else { // lowCase, w/o upCase
           if (pswCrit[1].numChar === "yes") { // with num
             if (pswCrit[1].specChar === "yes") { // with spec
               var characters = letters + numbers + special;
@@ -149,7 +114,7 @@ generateBtn.addEventListener("click", function(){
               console.log(result);
               return result;
             }
-          } else { // lowcase, w/o upcase and num
+          } else { // lowCase, w/o upCase and num
             if (pswCrit[1].specChar === "yes") { // with spec
               var characters = letters + special;
               randomForm();
@@ -163,8 +128,8 @@ generateBtn.addEventListener("click", function(){
             }
           }
         }
-      } else { // w/o lowcase
-        if (pswCrit[1].upCase === "yes") { // with upcase
+      } else { // w/o lowCase
+        if (pswCrit[1].upCase === "yes") { // with upCase
           if (pswCrit[1].numChar === "yes") { // with num
             if (pswCrit[1].specChar === "yes") { // with spec
               var characters = letters.toUpperCase() + numbers + special;
@@ -177,7 +142,7 @@ generateBtn.addEventListener("click", function(){
               console.log(result);
               return result;
             }
-          } else { // w/o lowcase, with upcase, w/o num
+          } else { // w/o lowCase, with upCase, w/o num
             if (pswCrit[1].specChar === "yes") { // with spec
               var characters = letters.toUpperCase() + special;
               randomForm();
@@ -190,7 +155,7 @@ generateBtn.addEventListener("click", function(){
               return result;
             }
           }
-        } else { // w/o lowcase and upcase
+        } else { // w/o lowCase and upCase
           if (pswCrit[1].numChar === "yes") { // with num
             if (pswCrit[1].specChar === "yes") { // with spec
               var characters = numbers + special;
@@ -203,7 +168,7 @@ generateBtn.addEventListener("click", function(){
               console.log(result);
               return result;
             }
-          } else { // w/o lowcase and upcase and num, only spec
+          } else { // w/o lowCase and upCase and num, only spec
             var characters = special;
             randomForm();
             console.log(result);
